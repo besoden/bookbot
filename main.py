ec5b0cc96ca69e91945f2459269adf9924e11453
besoden <nive.parthiban@gmail.com>
2025-05-30 22:22:36 -0700

    refactored to allow command line args

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from stats import get_book_word_count
 from stats import get_num_chars
 from stats import sort_char_nums
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents    
 
-def main(file_path):
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
@@ -21,4 +28,4 @@ def main(file_path):
         print(f"{d["char"]}: {d["num"]}")
     print("============= END ===============")
 
-main('books/frankenstein.txt')
+main()
